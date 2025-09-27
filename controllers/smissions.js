1daf3318666b927b75e4a37444b09b90bd4e76b7
benwasden <was24001@byui.edu>
2025-09-27 16:49:15 -0700

    Adding try/catch to soviet missions db

--- controllers/smissions.js
+++ controllers/smissions.js
@@ -3,81 +3,83 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger.tags=['Missions']
+    try {
     const result = await mongodb.getDatabase().db().collection('smissions').find();
-    result.toArray((err) => {
-        if (err) {
-            res.status(400).json({ message: err });
-        }
-    }
-    ).then((missions) => {
+    result.toArray().then((missions) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(missions);
     });
+    } catch (err) {
+        res.status(400).json({ message: err });
+    }
 };
 
 const getSingle = async (req, res) => {
     //#swagger.tags=['Missions']
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid mission id to see a mission.');
-    }
-    const missionId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('smissions').find({ _id: missionId });
-    result.toArray((err) => {
-        if (err) {
-            res.status(400).json({ message: err });
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json('Must use a valid mission id to see a mission.');
         }
-    }).then((missions) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(missions[0]);
-    });
+        const missionId = new ObjectId(req.params.id);
+        const result = await mongodb.getDatabase().db().collection('smissions').find({ _id: missionId });
+        result.toArray().then((missions) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(missions[0]);
+        });
+    } catch (err) {
+        res.status(400).json({ message: err });
+    }
 };
 
 const createMission = async (req, res) => {
     //#swagger.tags=['Missions']
-    const mission = {
-        program: req.body.program,
-        number: req.body.number,
-        launchDate: req.body.launchDate,
-        achievement: req.body.achievement
-    };
-    const response = await mongodb.getDatabase().db().collection('smissions').insertOne(mission);
-    if (response.acknowledged) {
-        res.status(204).send();
-    } else {
+    try {
+        const mission = {
+            program: req.body.program,
+            number: req.body.number,
+            launchDate: req.body.launchDate,
+            achievement: req.body.achievement
+        };
+        const response = await mongodb.getDatabase().db().collection('smissions').insertOne(mission);
+        if (response.acknowledged) {
+            res.status(204).send();
+    }} catch (err) {
         res.status(500).json(response.error || 'Some error occured while adding the mission.');
     };
 };
 
 const updateMission = async (req, res) => {
     //#swagger.tags=['Missions']
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid mission id to update a mission.');
-    }
-    const missionId = new ObjectId(req.params.id);
-    const mission = {
-        program: req.body.program,
-        number: req.body.number,
-        launchDate: req.body.launchDate,
-        achievement: req.body.achievement
-    };
-    const response = await mongodb.getDatabase().db().collection('smissions').replaceOne({ _id: missionId }, mission);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json('Must use a valid mission id to update a mission.');
+        }
+        const missionId = new ObjectId(req.params.id);
+        const mission = {
+            program: req.body.program,
+            number: req.body.number,
+            launchDate: req.body.launchDate,
+            achievement: req.body.achievement
+        };
+        const response = await mongodb.getDatabase().db().collection('smissions').replaceOne({ _id: missionId }, mission);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+    }} catch (err) {
         res.status(500).json(response.error || 'Some error occured while updating the mission.');
     };
 };
 
 const deleteMission = async (req, res) => {
     //#swagger.tags=['Missions']
-    if (!ObjectId.isValid(req.params.id)) {
-        res.status(400).json('Must use a valid mission id to delete a mission.');
-    }
-    const missionId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('smissions').deleteOne({ _id: missionId });
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
+    try {
+        if (!ObjectId.isValid(req.params.id)) {
+            res.status(400).json('Must use a valid mission id to delete a mission.');
+        }
+        const missionId = new ObjectId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('smissions').deleteOne({ _id: missionId });
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+    }} catch (err) {
         res.status(500).json(response.error || 'Some error occured while deleting the mission');
     };
 }
